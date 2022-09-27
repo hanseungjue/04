1e3094770422a8e9c5c436463cc6929acc32fe01
hanseungjue <hansj0628@naver.com>
2022-09-27 16:46:23 +0900

    task3

--- main.c
+++ main.c
@@ -4,24 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i,j;
-	int a,b,c,d,e;
-	printf("put two integers:");
-	scanf("%i %i",&i,&j);
+	int a,b,c;
+	printf("input the second:");
+	scanf("%i",&a);
 	
-	a=i+j;
-	printf("+ result is: %i\n",a);
+	b= a/60;
+	c= a%60;
 	
-	b=i-j;
-	printf("- result is: %i\n",b);
-	
-	c=i*j;
-	printf("* result is: %i\n",c);
-	
-	d=i/j;
-	printf("/ result is: %i\n",d);
-	
-	e=i%j;
-	printf("%% result is: %i",e);
+	printf("%i second is %i : %i",a,b,c);
 	return 0;
 }
