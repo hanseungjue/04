8b0633549e0389bb5ef5c02efd05f29c00d68020
hanseungjue <hansj0628@naver.com>
2022-09-27 16:36:56 +0900

    task2

--- main.c
+++ main.c
@@ -4,16 +4,24 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x, y, z, m;
-	int a, b, c;
-    x = 1;
-	y = 3; 
-	z = 5;
-	a = 3;
-	b = 4;
-	c = 5;
-	y=a*x*x+b*x+c;
-	m=(x+y+z)/3;
-	printf("y=%d, m=%d", y, m);
+	int i,j;
+	int a,b,c,d,e;
+	printf("put two integers:");
+	scanf("%i %i",&i,&j);
+	
+	a=i+j;
+	printf("+ result is: %i\n",a);
+	
+	b=i-j;
+	printf("- result is: %i\n",b);
+	
+	c=i*j;
+	printf("* result is: %i\n",c);
+	
+	d=i/j;
+	printf("/ result is: %i\n",d);
+	
+	e=i%j;
+	printf("%% result is: %i",e);
 	return 0;
 }
